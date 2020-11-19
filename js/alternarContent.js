9fb3ba5be721868ef3733145a35ab979af0009ad
Bernardo Pereira Oliveira <bernardo.258@hotmail.com>
2020-11-18 22:26:47 -0300

    Transição do slider

--- js/alternarContent.js
+++ js/alternarContent.js
@@ -3,6 +3,7 @@ const contentsAll = document.querySelectorAll('[data-content]');
 const links = document.querySelectorAll('[data-link]');
 
 const body = document.querySelector('body');
+const mascara = document.querySelector('#mascara');
 
 links.forEach(link => {
     link.addEventListener('click', alternarContent);
@@ -37,24 +38,28 @@ function esconderContent() {
             content.classList.add('desativado');
             content.classList.remove('ativo');
         })
-    }, 1100);
+    }, 1000);
 }
 
 function aparecerContentSelecionado(selecionado) {
 
+    // circle em 3000px, dps de 0.8s fica 0px, dps fica novamente com 3000px dps de 1s
+    body.style.clipPath = "circle(3000px at center)";
+    body.style.transition = "0.8s";
     body.style.clipPath = "circle(0px at center)";
 
     setTimeout(function () {
         body.style.clipPath = "circle(3000px at center)";
+        // mascara.style.backgroundColor = ""
 
-    }, 1000)
+    }, 1000);
 
     setTimeout(() => {
 
         // Abrir content selecionado
         contentsAll[Number(selecionado) - 1].classList.remove('desativado');
         contentsAll[Number(selecionado) - 1].classList.add('ativo');
-    }, 1100);
+    }, 1000);
 
 }
 
